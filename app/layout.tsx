12d600e7048e316fbb56b8aa4a7653360ce8fbd2
Luis Alberto Vásquez Vargas <luisv0909@gmail.com>
2026-05-06 13:22:59 -0300

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,11 @@ export const metadata = {
   description: "Shopping List Core UI",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
   return (
     <html lang="en">
       <body>
